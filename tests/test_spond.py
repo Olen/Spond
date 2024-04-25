0ab73f50c1e93eb9b1c7134970a8c3619e014fbb
Elliot <3186037+elliot-100@users.noreply.github.com>
2024-04-25 14:54:07 +0100

    tests: add tests for blank `id` in `get_event(id)`, `get_group(id)`

--- tests/test_spond.py
+++ tests/test_spond.py
@@ -83,6 +83,18 @@ async def test_get_event__no_match_raises_exception(mock_events, mock_token):
 
 
 @pytest.mark.asyncio
+async def test_get_event__blank_id_match_raises_exception(mock_events, mock_token):
+    """Test that a blank `id` raises IndexError."""
+
+    s = Spond(MOCK_USERNAME, MOCK_PASSWORD)
+    s.events = mock_events
+    s.token = mock_token
+
+    with pytest.raises(IndexError):
+        await s.get_event("")
+
+
+@pytest.mark.asyncio
 async def test_get_group__happy_path(mock_groups, mock_token):
     """Test that a valid `id` returns the matching group."""
 
@@ -107,3 +119,15 @@ async def test_get_group__no_match_raises_exception(mock_groups, mock_token):
 
     with pytest.raises(IndexError):
         await s.get_group("ID3")
+
+
+@pytest.mark.asyncio
+async def test_get_group__blank_id_raises_exception(mock_groups, mock_token):
+    """Test that a blank `id` raises IndexError."""
+
+    s = Spond(MOCK_USERNAME, MOCK_PASSWORD)
+    s.groups = mock_groups
+    s.token = mock_token
+
+    with pytest.raises(IndexError):
+        await s.get_group("")
