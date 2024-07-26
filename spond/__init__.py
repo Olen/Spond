5580c5fa2992127aee96767196e8b5bb8297d8cd
Elliot <3186037+elliot-100@users.noreply.github.com>
2024-07-26 17:53:10 +0100

    Use `type_extensions.TypeAlias` for Python < 3.10, else `typing.TypeAlias`

--- spond/__init__.py
+++ spond/__init__.py
@@ -1,6 +1,11 @@
+import sys
 from typing import Any, Dict
 
-from typing_extensions import TypeAlias
+if sys.version_info < (3, 10):
+    from typing_extensions import TypeAlias
+else:
+    from typing import TypeAlias
+
 
 DictFromJSON: TypeAlias = Dict[str, Any]
 """Simple type alias to annotate data retrieved from the API."""
