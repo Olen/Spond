180c52bb39fa4ac058345513f518bcc96da0e819
Elliot <3186037+elliot-100@users.noreply.github.com>
2024-09-14 11:37:37 +0100

    Refactor: remove unnecessary `elif` after `return` statement

--- spond/spond.py
+++ spond/spond.py
@@ -215,7 +215,7 @@ class Spond(_SpondBase):
 
         if chat_id is not None:
             return self._continue_chat(chat_id, text)
-        elif group_uid is None or user is None:
+        if group_uid is None or user is None:
             return {
                 "error": "wrong usage, group_id and user_id needed or continue chat with chat_id"
             }
