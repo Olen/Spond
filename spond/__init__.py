dd33f36893a3ccfe517cd8986a1086fadda8d4b1
elliot-100 <3186037+elliot-100@users.noreply.github.com>
2024-10-21 10:13:06 +0100

    Lint: upgrade deprecated `typing.Dict` to `dict`

--- spond/__init__.py
+++ spond/__init__.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Any, Dict
+from typing import Any
 
 if sys.version_info < (3, 10):
     from typing_extensions import TypeAlias
@@ -7,7 +7,7 @@ else:
     from typing import TypeAlias
 
 
-JSONDict: TypeAlias = Dict[str, Any]
+JSONDict: TypeAlias = dict[str, Any]
 """Simple alias for type hinting `dict`s that can be passed to/from JSON-handling functions."""
 
 
